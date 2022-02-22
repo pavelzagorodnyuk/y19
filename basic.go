4151d32fa503eecafe7e78da0110ffa1fd990b80
Pavel Zagorodnyuk <pavel.zagorodnyuk@gmail.com>
2022-02-22 12:43:10 +0300

    feat: add AreSimilar function
    AreSimilar reports whether selection1 and selection2 data selections
    are similar.

--- basic.go
+++ basic.go
@@ -66,6 +66,76 @@ func AreEqual(selection1, selection2 Data) bool {
 	return true
 }
 
+// AreSimilar reports whether selection1 and selection2 data selections are similar.
+func AreSimilar(selection1, selection2 Data, deviation float64) bool {
+
+	if selection1 == nil || selection2 == nil || math.IsNaN(deviation) || deviation < 0 {
+		return false
+	}
+
+	var length = selection1.Length()
+	var dimension = selection1.Dimension()
+
+	if length != selection2.Length() || dimension != selection2.Dimension() || length < 0 || dimension < 1 {
+		return false
+	}
+
+	// checking similarity of scales
+	for i := 0; i < dimension; i++ {
+		if selection1.Scale(i) != selection2.Scale(i) {
+			return false
+		}
+	}
+
+	var (
+		found  bool
+		marks  = make([]bool, length)
+		object = make([]interface{}, dimension)
+	)
+
+	// checking whether the selections contain similar objects
+	for i := 0; i < length; i++ {
+		found = false
+
+		// reading an object
+		for k := 0; k < dimension; k++ {
+			object[k] = selection1.Value(i, k)
+		}
+
+		// search for a similar object in the selection2
+	LOOP:
+		for j := 0; j < length && !found; j++ {
+			if marks[j] {
+				continue
+			}
+
+			for k := 0; k < dimension; k++ {
+
+				value1, ok1 := interfaceToFloat64(object[k])
+				value2, ok2 := interfaceToFloat64(selection2.Value(j, k))
+
+				if ok1 && ok2 {
+					if math.Abs(value2-value1) > deviation {
+						continue LOOP
+					}
+				} else if !reflect.DeepEqual(object[k], selection2.Value(j, k)) {
+					continue LOOP
+				}
+			}
+
+			found = true
+			marks[j] = true
+		}
+
+		// if a duplicate is not found, then finish the search
+		if !found {
+			return false
+		}
+	}
+
+	return true
+}
+
 func interfaceToFloat64(source interface{}) (float64, bool) {
 
 	var value float64 = math.NaN()
